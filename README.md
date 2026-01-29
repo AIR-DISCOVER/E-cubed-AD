3ad1174fbda26ae8de11985ea8dbd41b63ef4a40
kel <kelsulhgod@gmail.com>
2026-01-29 16:17:04 +0800

    E-cubed-AD based VAD model

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# E-cubed-AD
+## E-cubed-AD
---
+++ projects/mmdet3d_plugin/EAD/__init__.py
@@ -0,0 +1,8 @@
+from .modules import *
+from .runner import *
+from .hooks import *
+
+from .EAD import EAD
+from .EAD_head import EADHead
+from .VAD_transformer import VADPerceptionTransformer, \
+        CustomTransformerDecoder, MapDetectionTransformerDecoder
